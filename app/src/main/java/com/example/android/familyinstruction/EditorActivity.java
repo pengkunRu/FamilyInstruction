ab1c675cf8efdc06a4983ccbd0dae59f9da5ce69
rupengkun <522523828@qq.com>
2018-05-28 16:06:06 +0800

    1.09 在EditorActivity中添加菜单项

--- app/src/main/java/com/example/android/familyinstruction/EditorActivity.java
+++ app/src/main/java/com/example/android/familyinstruction/EditorActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.familyinstruction;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()){
+            case R.id.action_save:
+                return true;
+            case R.id.action_delete:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
