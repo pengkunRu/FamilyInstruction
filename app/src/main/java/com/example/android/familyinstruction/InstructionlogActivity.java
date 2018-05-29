28abc74ca6cabf7e3b2045634fd647dda5d526c3
rupengkun <522523828@qq.com>
2018-05-29 16:39:19 +0800

    1.27 去掉toolbar

--- app/src/main/java/com/example/android/familyinstruction/InstructionlogActivity.java
+++ app/src/main/java/com/example/android/familyinstruction/InstructionlogActivity.java
@@ -8,7 +8,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,9 +29,7 @@ public class InstructionlogActivity extends AppCompatActivity implements android
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        
         // Setup FAB to open EditorActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
