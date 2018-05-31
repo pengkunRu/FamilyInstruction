dce20ebdf74fc60309229059349544fb2fa490ad
rupengkun <522523828@qq.com>
2018-06-01 03:59:52 +0800

    1.37 实现Playing Streaming Video

--- app/src/main/java/com/example/android/familyinstruction/VedioDetailInformation.java
+++ app/src/main/java/com/example/android/familyinstruction/VedioDetailInformation.java
@@ -1,10 +1,11 @@
 package com.example.android.familyinstruction;
 
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class VedioDetailInformation extends AppCompatActivity {
@@ -17,16 +18,23 @@ public class VedioDetailInformation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vedio_detail_information);
 
-        mVedioView = (VideoView)findViewById(R.id.vedio_view);
-        mButton = (Button)findViewById(R.id.button);
+        mVedioView = (VideoView) findViewById(R.id.vedio_view);
+        mButton = (Button) findViewById(R.id.button);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String vedioPath = "android.resource://" + getPackageName() + "/" + R.raw.vedio_file1_1;
-                Uri uri = Uri.parse(vedioPath);
-                mVedioView.setVideoURI(uri);
-                mVedioView.start();
+                mVedioView.setVideoPath("http://p92t81y7d.bkt.clouddn.com/vedio_file1_1.mp4");
+                MediaController mediaController = new MediaController(VedioDetailInformation.this);
+                mediaController.setAnchorView(mVedioView);
+                mVedioView.setMediaController(mediaController);
+                mVedioView.requestFocus();
+                mVedioView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    // Close the progress bar and play the video
+                    public void onPrepared(MediaPlayer mp) {
+                        mVedioView.start();
+                    }
+                });
             }
         });
     }
