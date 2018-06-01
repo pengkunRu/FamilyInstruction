aad6115d789d196ad806f52b75d6d85818b32057
rupengkun <522523828@qq.com>
2018-06-01 15:28:50 +0800

    1.39 创建文本资源表的合约类

--- app/src/main/java/com/example/android/familyinstruction/data/InstructionContract.java
+++ app/src/main/java/com/example/android/familyinstruction/data/InstructionContract.java
@@ -42,9 +42,14 @@ public final class InstructionContract {
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_NOTES;
 
-        /** Name of database table for notes */
+        /** Name of database table for notes
+
+         // The MIME type for a single note.
+         public static final String CONTENT_ITEM_TYPE =
+         ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_NOTES;*/
         public final static String TABLE_NAME = "notes";
 
+        // 唯一标识符
         public final static String _ID = BaseColumns._ID;
         // 标题：这条家训选自
         public final static String COLUMN_NOTE_TITLE ="title";
@@ -52,5 +57,34 @@ public final class InstructionContract {
         public final static String COLUMN_NOTE_INSTRUCTION = "instruction";
         // 用户对这条家训的赏析
         public final static String COLUMN_NOTE_JUSTICE = "justice";
+    }
+
+
+    // 文本资源表的合约类内容
+    public final static class TextResourceEntry implements BaseColumns{
+
+        // 表名字
+        public static final String TABLE_NAME = "text_resource";
+
+        // 唯一标识符
+        public final static String _ID = BaseColumns._ID;
+        // 书籍名
+        public final static String COLUMN_BOOK_TITLE ="bookTitle";
+        // 书籍简介
+        public final static String COLUMN_BOOK_INTRODUCTION ="bookIntroduction";
+        // 书籍封面图片
+        public final static String COLUMN_BOOK_IMAGE ="bookImage";
+        // 作者名字
+        public final static String COLUMN_WRITER_NAME ="writerName";
+        // 作者简介
+        public final static String COLUMN_WRITER_INTRODUCTION ="writerIntroduction";
+        // 作者图片
+        public final static String COLUMN_WRITER_IMAGE ="bookImage";
+        // 家训文章类型
+        public final static String COLUMN_ARTICLE_TYPE ="articleType";
+        // 家训文章的文言文形式
+        public final static String ARTICLE_ANCIENT_FORMAT ="articleAncientFormat";
+        // 家训文章的白话文形式
+        public final static String  ARTICLE_VERNACULAR_FORMAT ="articleVernacularFormat";
     }
 }
