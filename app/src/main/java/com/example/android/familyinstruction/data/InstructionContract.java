4b567d1fcd8374c97c866d71e44a6d2252dc86f5
rupengkun <522523828@qq.com>
2018-06-01 21:30:56 +0800

    1.43 在合约类中实现文本资源表的content uri

--- app/src/main/java/com/example/android/familyinstruction/data/InstructionContract.java
+++ app/src/main/java/com/example/android/familyinstruction/data/InstructionContract.java
@@ -14,24 +14,24 @@ public final class InstructionContract {
 
     private InstructionContract(){}
 
-    // CONTENT_AUTHORITY
+    // CONTENT_AUTHORITY(统一资源定位符的中间部分)
     public static final String CONTENT_AUTHORITY = "com.example.android.familyinstruction";
 
-    /**
-     *  Base Content Uri
-     * 它将 URI 字符串作为输入，然后返回一个 URI
-     */
+    // BASE_CONTENT_URI（统一资源定位符的第一部分，第二部分）
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
-    // PATH_TableName
+    // PATH_TableName（用户鉴赏表）
     public static final String PATH_NOTES = "notes";
+    // PATH_TableName(文本资源表)
+    public static final String PATH_TEXT_RESOURCE = "text_resource";
 
 
+
+
+    // 用户鉴赏表的合约内容
     public final static class InstructionEntry implements BaseColumns{
 
-        /**
-         * The content URI to access the notes data in the provider
-         */
+        // content uri
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_NOTES);
 
         // The MIME type of for a list of notes
@@ -42,13 +42,8 @@ public final class InstructionContract {
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_NOTES;
 
-        /** Name of database table for notes
-
-         // The MIME type for a single note.
-         public static final String CONTENT_ITEM_TYPE =
-         ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_NOTES;*/
+        // 表名
         public final static String TABLE_NAME = "notes";
-
         // 唯一标识符
         public final static String _ID = BaseColumns._ID;
         // 标题：这条家训选自
@@ -60,8 +55,21 @@ public final class InstructionContract {
     }
 
 
+
+
     // 文本资源表的合约类内容
     public final static class TextResourceEntry implements BaseColumns{
+
+        // content uri
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_TEXT_RESOURCE);
+
+        // The MIME type of for a list of notes
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TEXT_RESOURCE;
+
+        // The MIME type for a single note.
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TEXT_RESOURCE;
 
         // 表名字
         public static final String TABLE_NAME = "text_resource";
