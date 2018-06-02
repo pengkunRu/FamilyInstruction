8a51ae36406d6e4d9cf22259c9bcb3d392336b09
rupengkun <522523828@qq.com>
2018-06-02 22:32:54 +0800

    1.48 古籍类界面增加导航栏

--- app/src/main/java/com/example/android/familyinstruction/BookShelfActivity.java
+++ app/src/main/java/com/example/android/familyinstruction/BookShelfActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.familyinstruction;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 
 import com.example.android.familyinstruction.data.InstructionContract.TextResourceEntry;
@@ -15,6 +18,42 @@ public class BookShelfActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_shelf);
+
+
+        /**
+         * 初始化导航栏信息
+         * 当前界面按钮为红色，其余为白色
+         * 当前界面按钮文字为白色，其余为红色
+         */
+        Button mNoteMaterial = findViewById(R.id.note_material_button2);
+        Button mTextMaterial = findViewById(R.id.text_material_button2);
+        Button mMediaMaterial = findViewById(R.id.media_material_button2);
+        mNoteMaterial.setBackgroundColor(getResources().getColor(R.color.navigationColor));
+        mNoteMaterial.setTextColor(getResources().getColor(R.color.colorPrimary));
+        mTextMaterial.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        mTextMaterial.setTextColor(getResources().getColor(R.color.navigationColor));
+        mMediaMaterial.setBackgroundColor(getResources().getColor(R.color.navigationColor));
+        mMediaMaterial.setTextColor(getResources().getColor(R.color.colorPrimary));
+
+        // 界面导航到用户札记界面
+        mNoteMaterial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BookShelfActivity.this, NoteMaterialActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // 界面导航到媒体资料界面
+        mMediaMaterial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BookShelfActivity.this, MediaMaterialActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
         // 获取数据源
