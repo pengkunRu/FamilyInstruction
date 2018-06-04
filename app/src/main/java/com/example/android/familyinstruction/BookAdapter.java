527faa0e9ec971c89e69859ca9a60470cebd18ad
rupengkun <522523828@qq.com>
2018-06-04 16:20:43 +0800

    1.58 添加了一些家训资源

--- app/src/main/java/com/example/android/familyinstruction/BookAdapter.java
+++ app/src/main/java/com/example/android/familyinstruction/BookAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -36,6 +37,8 @@ public class BookAdapter extends ArrayAdapter<Book> {
 
         ImageView imageView = (ImageView) listItemView.findViewById(R.id.book_image_view);
         imageView.setImageResource(currentItem.getImageResourceId());
+        TextView textView = (TextView)listItemView.findViewById(R.id.book_name_text_view);
+        textView.setText(currentItem.getBookTitle());
         return listItemView;
     }
 }
