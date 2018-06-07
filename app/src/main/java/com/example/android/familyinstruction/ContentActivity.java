caae771438e2c1e0a50cb4bf4b18bf73b33d4b89
rupengkun <522523828@qq.com>
2018-06-07 13:22:03 +0800

    1.67 翻译功能实现完毕

--- app/src/main/java/com/example/android/familyinstruction/ContentActivity.java
+++ app/src/main/java/com/example/android/familyinstruction/ContentActivity.java
@@ -6,7 +6,9 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.android.familyinstruction.data.InstructionContract.TextResourceEntry;
@@ -19,25 +21,55 @@ public class ContentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_content);
 
 
+        // 默认只显示文言文，不现实译文
+        TextView vernacularTextView = (TextView)findViewById(R.id.article_vernacular_tv);
+        vernacularTextView.setVisibility(View.GONE);
+
+
         // 获取用户想要浏览的章节在数据库中的唯一标识id
         Intent intent = getIntent();
         long id = intent.getExtras().getInt("id");
-        Log.i("Content","id: " + id);
+
 
         // 根据唯一标识符获取了对应的所有数据
         Content content = getContent(id);
 
         // 绑定视图，设置资源
         TextView typeTextView = (TextView)findViewById(R.id.article_type_text_view);
-        TextView translationTextView = (TextView)findViewById(R.id.article_type_text_view);
         TextView writerNameTextView = (TextView)findViewById(R.id.writer_name_text_view);
         TextView ancientTextView = (TextView)findViewById(R.id.article_ancient_tv);
-        TextView vernacularTextView = (TextView)findViewById(R.id.article_vernacular_tv);
+        vernacularTextView = (TextView)findViewById(R.id.article_vernacular_tv);
 
         typeTextView.setText(content.getArticleType());
         writerNameTextView.setText(content.getWriterName());
         ancientTextView.setText(content.getArticleAncientFormat());
         vernacularTextView.setText(content.getArticleVernacularFormat());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_content, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_translate:
+                if(item.getTitle() == getResources().getString(R.string.action_translate)){
+                    //显示原文，译文
+                    TextView vernacularTextView = (TextView)findViewById(R.id.article_vernacular_tv);
+                    vernacularTextView.setVisibility(View.VISIBLE);
+                    item.setTitle(getResources().getString(R.string.action_original_text));
+                }else{
+                    //显示原文
+                    TextView vernacularTextView = (TextView)findViewById(R.id.article_vernacular_tv);
+                    vernacularTextView.setVisibility(View.GONE);
+                    item.setTitle(getResources().getString(R.string.action_translate));
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
