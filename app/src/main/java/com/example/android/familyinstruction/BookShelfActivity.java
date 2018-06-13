9cf63b012907ce1169b33e8097e1b907e1c17592
rupengkun <522523828@qq.com>
2018-06-13 11:39:19 +0800

    1.85 将NoteMaterialActivity的逻辑 转移到了 BookShelfActivity

--- app/src/main/java/com/example/android/familyinstruction/BookShelfActivity.java
+++ app/src/main/java/com/example/android/familyinstruction/BookShelfActivity.java
@@ -1,25 +1,40 @@
 package com.example.android.familyinstruction;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.android.familyinstruction.data.InstructionContract.UserInfoEntry;
 import com.example.android.familyinstruction.data.InstructionContract.TextResourceEntry;
 
 import java.util.ArrayList;
 
-public class BookShelfActivity extends AppCompatActivity {
+public class BookShelfActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+
+    // NavigationView Header Image
+    private ImageView mHeaderImageView;
+    // NavigationView Header Textview
+    private TextView mHeaderUserName;
+    private Menu mMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +46,22 @@ public class BookShelfActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(this);
+        }
+        // 引入头布局和菜单布局
+        mNavigationView.inflateHeaderView(R.layout.header);
+        mNavigationView.inflateMenu(R.menu.menu_drawer);
+        // 获取头部布局
+        View NavHeaderView = mNavigationView.getHeaderView(0);
+        // 获取菜单布局
+        mMenu = mNavigationView.getMenu();
+
+        mHeaderImageView = (ImageView)NavHeaderView.findViewById(R.id.user_photo_header);
+        mHeaderUserName = (TextView)NavHeaderView.findViewById(R.id.user_name_header);
 
 
         /**
@@ -97,6 +128,20 @@ public class BookShelfActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        if(getUserStatus()==0){
+            mHeaderImageView.setImageResource(R.drawable.ic_logout_photo);
+            mHeaderUserName.setText("");
+            mMenu.findItem(R.id.logout).setTitle("登陆");
+        }else {
+            mHeaderImageView.setImageResource(R.drawable.ic_login_photo);
+            mHeaderUserName.setText(getUserName());
+            mMenu.findItem(R.id.logout).setTitle("退出登录");
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(mToggle.onOptionsItemSelected(item)){
             return true;
@@ -130,5 +175,143 @@ public class BookShelfActivity extends AppCompatActivity {
         }
 
         return books;
+    }
+
+    /**
+     * TODO 侧滑抽屉布局中菜单项的点击处理
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                logout();
+                break;
+            case R.id.bookshelf:
+                if(getUserStatus()==0){
+                    // 用户想要登陆家训应用
+                    Intent intent = new Intent(BookShelfActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+                break;
+            case R.id.vedio_collection:
+                if(getUserStatus()==0){
+                    // 用户想要登陆家训应用
+                    Intent intent = new Intent(BookShelfActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+                break;
+            case R.id.search:
+                if(getUserStatus()==0){
+                    // 用户想要登陆家训应用
+                    Intent intent = new Intent(BookShelfActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+                break;
+            case R.id.action_review:
+                if(getUserStatus()==0){
+                    // 用户想要登陆家训应用
+                    Intent intent = new Intent(BookShelfActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+                break;
+            case R.id.settings:
+                if(getUserStatus()==0){
+                    // 用户想要登陆家训应用
+                    Intent intent = new Intent(BookShelfActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+                break;
+        }
+
+        item.setChecked(true);
+        mDrawerLayout.closeDrawers();
+        return true;
+    }
+
+
+    // TODO 辅助函数 获取用户名
+    private String getUserName(){
+        String[] projection = {
+                UserInfoEntry.COLUMN_USER_NAME
+        };
+
+        Cursor cursor = getContentResolver().query(UserInfoEntry.CONTENT_URI, projection,null,null,null);
+
+        String userName = "";
+        int userNameColumnIndex = cursor.getColumnIndex(UserInfoEntry.COLUMN_USER_NAME);
+        while (cursor.moveToNext()){
+            userName = cursor.getString(userNameColumnIndex);
+        }
+        return userName;
+    }
+    // TODO 辅助函数 获取用户密码
+    private String getUserPassword(){
+        String[] projection = {
+                UserInfoEntry.COLUMN_USER_PASSWORD
+        };
+
+        Cursor cursor = getContentResolver().query(UserInfoEntry.CONTENT_URI, projection,null,null,null);
+
+        String userPassword = "";
+        int userPasswordColumnIndex = cursor.getColumnIndex(UserInfoEntry.COLUMN_USER_PASSWORD);
+        while (cursor.moveToNext()){
+            userPassword = cursor.getString(userPasswordColumnIndex);
+        }
+        return userPassword;
+    }
+    // TODO 辅助函数 获取用户登陆状态
+    private int getUserStatus(){
+        String[] projection = {
+                UserInfoEntry.COLUMN_USER_STATUS
+        };
+
+        Cursor cursor = getContentResolver().query(UserInfoEntry.CONTENT_URI, projection,null,null,null);
+
+        int userStatus = -1;
+        int userStatusColumnIndex = cursor.getColumnIndex(UserInfoEntry.COLUMN_USER_STATUS);
+        while (cursor.moveToNext()){
+            userStatus = cursor.getInt(userStatusColumnIndex);
+        }
+        Log.i("Note","用户登录状态："+userStatus);
+        return userStatus;
+    }
+    // TODO 辅助函数 更新用户用户状态
+    private void changeUserStatus(){
+        ContentValues values = new ContentValues();
+        if(getUserStatus()==0){
+            values.put(UserInfoEntry.COLUMN_USER_STATUS,1);
+        }else{
+            values.put(UserInfoEntry.COLUMN_USER_STATUS,0);
+        }
+
+        int rowsAffected = getContentResolver().update(UserInfoEntry.CONTENT_URI,values,null,null);
+        if (rowsAffected == 0) {
+            Toast.makeText(this, getString(R.string.user_log_out_failed),
+                    Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, getString(R.string.user_log_out_successful),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    // TODO 管理用户的登录-退出（辅助函数）
+    private void logout(){
+        if(getUserStatus()==0){
+            // 用户想要登陆家训应用
+            Intent intent = new Intent(BookShelfActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }else{
+            changeUserStatus();
+            if(getUserStatus()==0){
+                mHeaderImageView.setImageResource(R.drawable.ic_logout_photo);
+                mHeaderUserName.setText("");
+                mMenu.findItem(R.id.logout).setTitle("登陆");
+            }else {
+                mHeaderImageView.setImageResource(R.drawable.ic_login_photo);
+                mHeaderUserName.setText(getUserName());
+                mMenu.findItem(R.id.logout).setTitle("退出登录");
+            }
+        }
     }
 }
